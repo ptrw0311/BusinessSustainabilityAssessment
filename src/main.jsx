8ecc4d97340f155c54aa2cede2a40c6ed09efa5d
ptrw0311 <ptrw0311@gmail.com>
2025-09-02 01:58:35 +0000

    修正後打包

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.css' // ✅ 保持引入，Tailwind 會在建置時展開
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
